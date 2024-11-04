c8ac97e840b3cbcb9779ea74c83978d6252a3155
MdTanzil <mdtanzil.py@gmail.com>
2024-11-05 02:23:52 +0600

    search and back previous data enable

--- src/products/Search.jsx
+++ src/products/Search.jsx
@@ -1,4 +1,32 @@
+import { useContext, useEffect, useState, useRef } from "react";
+import { ProductContext } from "../context";
+
 const Search = () => {
+  const [search, setSearch] = useState("");
+  const { products, setProducts } = useContext(ProductContext);
+  const pref = useRef([]);
+  useEffect(() => {
+    if (pref.current.length === 0 && products.length > 0) {
+      pref.current = [...products];
+      console.log("Original products stored:", pref.current);
+    }
+  }, [products]);
+
+  useEffect(() => {
+    if (search.length == 0) {
+      if (products !== pref.current) {
+        setProducts(pref.current);
+      }
+      // console.log(products);
+    } else {
+      // console.log(products);
+      const filtered = (pref.current || []).filter((product) =>
+        product.title.toLowerCase().includes(search.toLowerCase())
+      );
+      setProducts(filtered);
+    }
+  }, [products, search, setProducts]);
+
   return (
     <div className="flex flex-1 items-center px-3.5 py-2 text-gray-400 group hover:ring-1 hover:ring-gray-300 focus-within:!ring-2 ring-inset focus-within:!ring-teal-500 rounded-md">
       <svg
@@ -24,6 +52,8 @@ const Search = () => {
         aria-autocomplete="list"
         defaultValue
         style={{ caretColor: "rgb(107, 114, 128)" }}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
     </div>
   );
